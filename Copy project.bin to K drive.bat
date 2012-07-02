9d0ba77887ab37ccaf264122c7e70792367a92e0
Gavin Clarke <ggaavviinnccllaarrkkee@hotmail.com>
2012-07-02 09:28:36 +0100

    bat file to work more often

--- Copy project.bin to K drive.bat
+++ Copy project.bin to K drive.bat
@@ -1,6 +1,6 @@
 
-cd C:\Users\%USERNAME%\git\Equinox-Clock\Equinox
+cd %HOMEDRIVE%%HOMEPATH%\git\Equinox-Clock\Equinox
 make
 del K:\firmware.bin
-copy C:\Users\%USERNAME%\git\Equinox-Clock\Equinox\FLASH_RUN\project.bin K:\
+copy %HOMEDRIVE%%HOMEPATH%\git\Equinox-Clock\Equinox\FLASH_RUN\project.bin K:\
 pause
