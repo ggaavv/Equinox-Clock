888fa23d6d5cb84b12385f1799f84c68d4d2717d
Gavin Clarke <ggaavviinnccllaarrkkee@hotmail.com>
2012-07-04 11:14:02 +0100

    Use _BIT macro

--- Equinox/pinout.h
+++ Equinox/pinout.h
@@ -36,38 +36,38 @@
 //For Equinox clock board
 #define LED_LE_PORT			0
 #define LED_LE_PIN			16
-#define LED_LE_BIT			(1 << LED_CS_PIN)
+#define LED_LE_BIT			_BIT(LED_CS_PIN)
 #define LED_OE_PORT			0
 #define LED_OE_PIN			19
-#define LED_OE_BIT			(1 << LED_LE_PIN)
+#define LED_OE_BIT			_BIT(LED_LE_PIN)
 #define LED_SCK_PORT     	0
 #define LED_SCK_PIN			15
-#define LED_SCK_BIT			(1 << LED_SCK_PIN)
+#define LED_SCK_BIT			_BIT(LED_SCK_PIN)
 #define LED_MISO_PORT		0
 #define LED_MISO_PIN		17
-#define LED_MISO_BIT		(1 << LED_MISO_PIN)
+#define LED_MISO_BIT		_BIT(LED_MISO_PIN)
 #define LED_MOSI_PORT		0
 #define LED_MOSI_PIN		18
-#define LED_MOSI_BIT		(1 << LED_MOSI_PIN)
+#define LED_MOSI_BIT		_BIT(LED_MOSI_PIN)
 
 #define LED_SPI_CHN			0
 */
 //For DEV board
 #define LED_LE_PORT			0
 #define LED_LE_PIN			6
-#define LED_LE_BIT			(1 << LED_LE_PIN)
+#define LED_LE_BIT			_BIT(LED_LE_PIN)
 #define LED_OE_PORT			0//not connected but can use
 #define LED_OE_PIN			10
-#define LED_OE_BIT			(1 << LED_LE_PIN)
+#define LED_OE_BIT			_BIT(LED_LE_PIN)
 #define LED_SCK_PORT     	0
 #define LED_SCK_PIN			7
-#define LED_SCK_BIT			(1 << LED_SCK_PIN)
+#define LED_SCK_BIT			_BIT(LED_SCK_PIN)
 #define LED_MISO_PORT		0
 #define LED_MISO_PIN		8
-#define LED_MISO_BIT		(1 << LED_MISO_PIN)
+#define LED_MISO_BIT		_BIT(LED_MISO_PIN)
 #define LED_MOSI_PORT		0
 #define LED_MOSI_PIN		9
-#define LED_MOSI_BIT		(1 << LED_MOSI_PIN)
+#define LED_MOSI_BIT		_BIT(LED_MOSI_PIN)
 
 #define LED_SPI_CHN			0
 
@@ -76,59 +76,59 @@
 //For Equinox clock board
 #define WF_CS_PORT			0
 #define WF_CS_PIN			6
-#define WF_CS_BIT			(1 <<  WF_CS_PIN)
+#define WF_CS_BIT			_BIT(WF_CS_PIN)
 #define WF_RESET_PORT		0
 #define WF_RESET_PIN		11
-#define WF_RESET_BIT		(1 << WF_RESET_PIN)
+#define WF_RESET_BIT		_BIT(WF_RESET_PIN)
 #define WF_HIBERNATE_PORT	0
 #define WF_HIBERNATE_PIN	10
-#define WF_HIBERNATE_BIT	(1 << WF_HIBERNATE_PIN)
+#define WF_HIBERNATE_BIT	_BIT(WF_HIBERNATE_PIN)
 #define WF_SCK_PORT     	0
 #define WF_SCK_PIN			7
-#define WF_SCK_BIT			(1 <<  WF_SCK_PIN)
+#define WF_SCK_BIT			_BIT(WF_SCK_PIN)
 #define WF_MISO_PORT		0
 #define WF_MISO_PIN			8
-#define WF_MISO_BIT			(1 <<  WF_MISO_PIN)
+#define WF_MISO_BIT			_BIT(WF_MISO_PIN)
 #define WF_MOSI_PORT		0
 #define WF_MOSI_PIN			9
-#define WF_MOSI_BIT			(1 <<  WF_MOSI_PIN)
+#define WF_MOSI_BIT			_BIT(WF_MOSI_PIN)
 #define WF_EINT0_PORT		2
 #define WF_EINT0_PIN		10
-#define WF_EINT0_BIT		(1 <<  WF_EINT0_PIN)
+#define WF_EINT0_BIT		_BIT(WF_EINT0_PIN)
 #define WF_EINT2_PORT		2
 #define WF_EINT2_PIN		12
-#define WF_EINT2_BIT		(1 <<  WF_EINT2_PIN)
+#define WF_EINT2_BIT		_BIT(WF_EINT2_PIN)
 */
 
 //For DEV board
 #define WF_CS_PORT			0
 #define WF_CS_PIN			16
-#define WF_CS_BIT			(1 << WF_CS_PIN)
+#define WF_CS_BIT			_BIT(WF_CS_PIN)
 //Reset connected to LPC reset in
 #define WF_RESET_PORT		0
 #define WF_RESET_PIN		11
-#define WF_RESET_BIT		(1 << WF_RESET_PIN)
+#define WF_RESET_BIT		_BIT(WF_RESET_PIN)
 #define WF_HIBERNATE_PORT	0
 #define WF_HIBERNATE_PIN	23
-#define WF_HIBERNATE_BIT	(1 << WF_HIBERNATE_PIN)
+#define WF_HIBERNATE_BIT	_BIT(WF_HIBERNATE_PIN)
 #define WF_SCK_PORT     	0
 #define WF_SCK_PIN			15
-#define WF_SCK_BIT			(1 <<  WF_SCK_PIN)
+#define WF_SCK_BIT			_BIT(WF_SCK_PIN)
 #define WF_MISO_PORT		0
 #define WF_MISO_PIN			17
-#define WF_MISO_BIT			(1 <<  WF_MISO_PIN)
+#define WF_MISO_BIT			_BIT(WF_MISO_PIN)
 #define WF_MOSI_PORT		0
 #define WF_MOSI_PIN			18
-#define WF_MOSI_BIT			(1 <<  WF_MOSI_PIN)
+#define WF_MOSI_BIT			_BIT(WF_MOSI_PIN)
 #define WF_EINT0_PORT		2
 #define WF_EINT0_PIN		10
-#define WF_EINT0_BIT		(1 <<  WF_EINT0_PIN)
+#define WF_EINT0_BIT		_BIT(WF_EINT0_PIN)
 #define WF_EINT2_PORT		2
 #define WF_EINT2_PIN		12
-#define WF_EINT2_BIT		(1 <<  WF_EINT2_PIN)
+#define WF_EINT2_BIT		_BIT(WF_EINT2_PIN)
 #define WF_EINT3_PORT		2//Using this one
 #define WF_EINT3_PIN		13 //Using this one
-#define WF_EINT3_BIT		(1 <<  WF_EINT3_PIN) //Using this one
+#define WF_EINT3_BIT		_BIT(WF_EINT3_PIN) //Using this one
 
 #define WF_SPI_CHN			1
 #define WF_INT				1
@@ -137,25 +137,25 @@
 //      TMP100 Pins
 #define TMP100_SDA1_PORT	0
 #define TMP100_SDA1_PIN		0
-#define TMP100_SDA1_BIT		(1 <<  TMP100_SDA1_PIN)
+#define TMP100_SDA1_BIT		_BIT(TMP100_SDA1_PIN)
 #define TMP100_SLC1_PORT	0
 #define TMP100_SLC1_PIN		1
-#define TMP100_SLC1_BIT		(1 <<  TMP100_SLC1_PIN)
+#define TMP100_SLC1_BIT		_BIT(TMP100_SLC1_PIN)
 
 
 //		LPCMini LED's
 #define LED1_PORT			0
 #define LED1_PIN			18
-#define LED1_BIT			(1 <<  LED1_PIN)
+#define LED1_BIT			_BIT(LED1_PIN)
 #define LED2_PORT			0
 #define LED2_PIN			20
-#define LED2_BIT			(1 <<  LED2_PIN)
+#define LED2_BIT			_BIT(LED2_PIN)
 #define LED3_PORT			0
 #define LED3_PIN			21
-#define LED3_BIT			(1 <<  LED3_PIN)
+#define LED3_BIT			_BIT(LED3_PIN)
 #define LED4_PORT			0
 #define LED4_PIN			23
-#define LED4_BIT			(1 <<  LED4_PIN)
+#define LED4_BIT			_BIT(LED4_PIN)
 
 
 #endif  /* _PINOUT_H */
